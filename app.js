861ac6e49befa87bdaa5faa53f52b608b7343ba2
Fedor Indutny <fedor.indutny@gmail.com>
2013-08-17 20:24:37 +0400

    app: correct usage of nconf

--- app.js
+++ app.js
@@ -1,32 +1,49 @@
-var nconf = require('nconf');
 var cluster = require('cluster');
 
-nconf.argv()
-     .env()
-     .file({ file: 'config.json' });
-
-nconf.set('server', 'dns1');
-nconf.set('probe:names', 'request');
-nconf.set('probe:interval', '60000');
-nconf.set('db:hostname', '127.0.0.1');
-nconf.set('db:port', 6379);
-nconf.set('db:prefix:probe', 'p');
-nconf.set('db:prefix:domain', 'd');
-nconf.set('db:prefix:session', 's');
-nconf.set('db:prefix:token', 't');
-nconf.set('db:prefix:user', 'u');
-nconf.set('api:ssl:cert', __dirname + '/keys/cert.pem');
-nconf.set('api:ssl:key', __dirname + '/keys/key.pem');
-nconf.set('api:pbkdf2:saltlen', 16);
-nconf.set('api:pbkdf2:iterations', 100);
-nconf.set('api:pbkdf2:length', 20);
-nconf.set('api:token:length', 20);
-nconf.set('api:token:ttl', 3600);
-nconf.set('api:port', 44300);
-nconf.set('api:hostname', '0.0.0.0');
-nconf.set('api:workers', 1);
-nconf.set('dns:port', 5959);
-nconf.set('dns:hostname', '127.0.0.1');
+var nconf = require('nconf')
+   .argv()
+   .env()
+   .file({ file: 'config.json' })
+   .defaults({
+     server: 'dns1',
+     probe: {
+       names: 'request',
+       interval: 60000
+     },
+     db: {
+       hostname: '127.0.0.1',
+       port: 6379,
+       prefix: {
+         probe: 'p',
+         domain: 'd',
+         session: 's',
+         token: 't',
+         user: 'u'
+       }
+     },
+     api: {
+       ssl: {
+         cert: __dirname + '/keys/cert.pem',
+         key: __dirname + '/keys/key.pem'
+       },
+       pbkdf2: {
+         saltlen: 16,
+         iterations: 100,
+         length: 20
+       },
+       token: {
+         length: 20,
+         ttl: 3600
+       },
+       port: 44300,
+       hostname: '127.0.0.1',
+       workers: 1
+     },
+     dns: {
+       port: 5959,
+       hostname: '127.0.0.1'
+     }
+   });
 
 var api = require('./lib/api');
 var dns = require('./lib/dns');
